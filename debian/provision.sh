983c4c6b789e6d1b0bcb3e325976d3d0e63a1071
c33s <dev@c33s.net>
2016-10-07 17:06:33 +0200

    initial commit

---
+++ debian/provision.sh
@@ -0,0 +1,53 @@
+﻿#!/bin/bash
+########################################################################################################################
+# Base Setup
+########################################################################################################################
+apt-get update && apt-get install --quiet --yes --force-yes --no-install-recommends \
+    apt-utils \
+    ca-certificates \
+    curl \
+    git \
+    grep \
+    lynx \
+    openssh-client \
+    rsync \
+    software-properties-common \
+    unzip \
+    wget \
+    zip \
+    && apt-get clean -qq \
+    && apt-get autoremove -qq \
+    && ( -rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* >/dev/null 2>&1 || true )
+########################################################################################################################
+# DotDeb Repo
+########################################################################################################################
+cat << EOF > /etc/apt/sources.list.d/dotdeb.list
+    deb http://packages.dotdeb.org jessie all
+    deb-src http://packages.dotdeb.org jessie all
+EOF
+wget --no-clobber --quiet https://www.dotdeb.org/dotdeb.gpg \
+    && apt-key add dotdeb.gpg \
+    && apt-get update \
+    && rm dotdeb.gpg
+
+########################################################################################################################
+# ssh
+########################################################################################################################
+mkdir -p ~/.ssh \
+    && ssh-keyscan -H github.com >> ~/.ssh/known_hosts \
+    && ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts \
+    && ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts \
+    && cat << EOF > ~/.ssh/config
+Host github.com
+    HostName github.com
+    User git
+    Port 22
+Host gitlab.com
+    HostName gitlab.com
+    User git
+    Port 22
+Host bitbucket.org
+    HostName bitbucket.org
+    User git
+    Port 22
+EOF
---
+++ openwrt-build/provision.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+########################################################################################################################
+# Base Setup
+########################################################################################################################
+apt-get update && apt-get install --quiet --yes --force-yes --no-install-recommends \
+    git-core \
+    build-essential \
+    libssl-dev \
+    libncurses5-dev \
+    unzip \
+    gawk \
+    subversion \
+    mercurial \
+    && apt-get clean -qq \
+    && apt-get autoremove -qq \
+    && ( -rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* >/dev/null 2>&1 || true )
