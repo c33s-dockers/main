1daea6e5428d3b1160621f70049cc2cbde651c80
c33s <dev@c33s.net>
2017-07-29 02:45:29 +0200

    refactored repo (wip) refs: #1

---
+++ debian/9-stretch/provision.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+########################################################################################################################
+#  Ondřej Surý Repo
+########################################################################################################################
+cat << EOF > /etc/apt/sources.list.d/php.list
+    deb https://packages.sury.org/php/ $(lsb_release -sc) main
+EOF
+wget --no-clobber --quiet -O ./php.gpg https://packages.sury.org/php/apt.gpg \
+    && apt-key add php.gpg \
+    && apt-get update \
+    && rm php.gpg
+
+
--- debian/baseprovision.sh
+++ debian/baseprovision.sh
@@ -1,13 +1,16 @@
-﻿#!/bin/bash
+#!/bin/bash
 ########################################################################################################################
 # Base Setup
 ########################################################################################################################
 apt-get update && apt-get install --quiet --yes --force-yes --no-install-recommends \
+    apt-transport-https \
     apt-utils \
     ca-certificates \
     curl \
     git \
     grep \
+    lftp \
+    lsb-release \
     lynx \
     openssh-client \
     rsync \
@@ -15,29 +18,16 @@ apt-get update && apt-get install --quiet --yes --force-yes --no-install-recomme
     unzip \
     wget \
     zip \
-    lftp \
-    openssh-client \
     && apt-get clean -qq \
     && apt-get autoremove -qq \
     && ( -rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* >/dev/null 2>&1 || true )
-########################################################################################################################
-# DotDeb Repo
-########################################################################################################################
-cat << EOF > /etc/apt/sources.list.d/dotdeb.list
-    deb http://packages.dotdeb.org jessie all
-    deb-src http://packages.dotdeb.org jessie all
-EOF
-wget --no-clobber --quiet https://www.dotdeb.org/dotdeb.gpg \
-    && apt-key add dotdeb.gpg \
-    && apt-get update \
-    && rm dotdeb.gpg
 
 ########################################################################################################################
 # ssh
 ########################################################################################################################
 mkdir -p ~/.ssh \
-    && ssh-keyscan -H github.com >> ~/.ssh/known_hosts \
     && ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts \
+    && ssh-keyscan -H github.com >> ~/.ssh/known_hosts \
     && ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts \
     && cat << EOF > ~/.ssh/config
 Host github.com
