2c6754a6817e750222871fd07f55e1a15edb29cd
c33s <dev@c33s.net>
2017-07-29 03:26:29 +0200

    wip

--- debian/baseprovision.sh
+++ debian/baseprovision.sh
@@ -2,12 +2,13 @@
 ########################################################################################################################
 # Base Setup
 ########################################################################################################################
-apt-get update && apt-get install --quiet --yes --force-yes --no-install-recommends \
+apt-get update && apt-get install --quiet --yes --no-install-recommends \
     apt-transport-https \
     apt-utils \
     ca-certificates \
     curl \
     git \
+    gpgv2 \
     grep \
     lftp \
     lsb-release \
@@ -25,21 +26,21 @@ apt-get update && apt-get install --quiet --yes --force-yes --no-install-recomme
 ########################################################################################################################
 # ssh
 ########################################################################################################################
-mkdir -p ~/.ssh \
-    && ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts \
-    && ssh-keyscan -H github.com >> ~/.ssh/known_hosts \
-    && ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts \
-    && cat << EOF > ~/.ssh/config
-Host github.com
-    HostName github.com
-    User git
-    Port 22
-Host gitlab.com
-    HostName gitlab.com
-    User git
-    Port 22
-Host bitbucket.org
-    HostName bitbucket.org
-    User git
-    Port 22
-EOF
+#mkdir -p ~/.ssh \
+#    && ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts \
+#    && ssh-keyscan -H github.com >> ~/.ssh/known_hosts \
+#    && ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts \
+#    && cat << EOF > ~/.ssh/config
+#Host github.com
+#    HostName github.com
+#    User git
+#    Port 22
+#Host gitlab.com
+#    HostName gitlab.com
+#    User git
+#    Port 22
+#Host bitbucket.org
+#    HostName bitbucket.org
+#    User git
+#    Port 22
+#EOF
