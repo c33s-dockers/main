99fdf454c76f7e563f97047c7b7844fce68501c8
c33s <dev@c33s.net>
2017-07-31 00:17:20 +0200

    wip

--- debian/provision-base.sh
+++ debian/provision-base.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
-
-########################################################################################################################
-# Base Setup
-########################################################################################################################
+﻿#!/bin/bash
 apt-get update && apt-get install --quiet --yes --no-install-recommends \
-    apt-transport-https \
     apt-utils \
     ca-certificates \
-    bash \
+    curl \
     git \
-    gpgv2 \
     grep \
-    lsb-release \
+    lynx \
+    openssh-client \
+    rsync \
+    software-properties-common \
+    unzip \
+    wget \
+    zip \
     && apt-get clean -qq \
     && apt-get autoremove -qq \
     && ( -rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* >/dev/null 2>&1 || true )
-cd root
-pwd
-ls -lsa
+
 ########################################################################################################################
 # ssh
 ########################################################################################################################
+#ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
+#ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
 #mkdir -p ~/.ssh \
-#    && ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts \
 #    && ssh-keyscan -H github.com >> ~/.ssh/known_hosts \
+#    && ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts \
 #    && ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts \
 #    && cat << EOF > ~/.ssh/config
 #Host github.com
@@ -39,13 +39,3 @@ ls -lsa
 #    User git
 #    Port 22
 #EOF
-
-#    curl \    
-#    lftp \
-#    lynx \
-#    openssh-client \
-#    rsync \
-#    software-properties-common \
-#    unzip \
-#    wget \
-#    zip \
