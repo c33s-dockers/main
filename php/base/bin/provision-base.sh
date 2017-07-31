6f115b0ea00aae42c91dfc02c080f17d055e566f
c33s <dev@c33s.net>
2017-07-31 04:57:13 +0200

    reenabled xdebug in php/base/bin/provision-base.sh

--- php/base/bin/provision-base.sh
+++ php/base/bin/provision-base.sh
@@ -60,8 +60,8 @@ apt-get update && apt-get install --quiet --yes --no-install-recommends \
         php$DOCKER_PHP_VERSION-zip
 apt-get clean -qq
 apt-get autoremove -qq && ( -rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* >/dev/null 2>&1 || true ) \
-    && ( echo 'date.timezone="Europe/Vienna"' >> /etc/php/$DOCKER_PHP_VERSION/cli/conf.d/timezone.ini || true ) \
-    && rm -rf /etc/php/$DOCKER_PHP_VERSION/cli/conf.d/20-xdebug.ini
+    && ( echo 'date.timezone="Europe/Vienna"' >> /etc/php/$DOCKER_PHP_VERSION/cli/conf.d/timezone.ini || true )
+#    && rm -rf /etc/php/$DOCKER_PHP_VERSION/cli/conf.d/20-xdebug.ini
 
 
 ########################################################################################################################
