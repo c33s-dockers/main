65eedd563facd6a12fa86fb541cb96d6524d033a
c33s <dev@c33s.net>
2017-07-31 04:31:42 +0200

    wip

--- debian/bin/provision-stretch.sh
+++ debian/bin/provision-stretch.sh
@@ -4,16 +4,8 @@
 ########################################################################################################################
 echo "running provision for"
 figlet $(lsb_release -sc)
-########################################################################################################################
-#  Sury Repo
-########################################################################################################################
-cat << EOF > /etc/apt/sources.list.d/php.list
-deb https://packages.sury.org/php/ $(lsb_release -sc) main
-EOF
-wget --no-clobber --quiet -O ./php.gpg https://packages.sury.org/php/apt.gpg
-apt-key add php.gpg
-apt-get update
-rm php.gpg
+
+echo "nothing to do for wheezy right now"
 
 
 
---
+++ php/base/bin/provision-base.sh
@@ -0,0 +1,84 @@
+﻿#!/bin/bash
+
+if [ "$(lsb_release -sc)" = "stretch" ]; then
+    ########################################################################################################################
+    #  Sury Repo
+    ########################################################################################################################
+    cat << EOF > /etc/apt/sources.list.d/php.list
+        deb https://packages.sury.org/php/ $(lsb_release -sc) main
+EOF
+    wget --no-clobber --quiet -O ./php.gpg https://packages.sury.org/php/apt.gpg
+    apt-key add php.gpg
+    apt-get update
+    rm php.gpg
+else
+    ########################################################################################################################
+    # DotDeb Repo
+    ########################################################################################################################
+    cat << EOF > /etc/apt/sources.list.d/dotdeb.list
+        deb http://packages.dotdeb.org jessie all
+        deb-src http://packages.dotdeb.org jessie all
+EOF
+    wget --no-clobber --quiet https://www.dotdeb.org/dotdeb.gpg \
+        && apt-key add dotdeb.gpg \
+        && apt-get update \
+        && rm dotdeb.gpg
+fi
+
+apt-get update && apt-get install --quiet --yes --no-install-recommends \
+        php$DOCKER_PHP_VERSION-cli \
+        php$DOCKER_PHP_VERSION-apcu \
+        php$DOCKER_PHP_VERSION-apcu-bc \
+        php$DOCKER_PHP_VERSION-bcmath \
+        php$DOCKER_PHP_VERSION-bz2 \
+        php$DOCKER_PHP_VERSION-common \
+        php$DOCKER_PHP_VERSION-curl \
+        php$DOCKER_PHP_VERSION-enchant \
+        php$DOCKER_PHP_VERSION-gd \
+        php$DOCKER_PHP_VERSION-geoip \
+        php$DOCKER_PHP_VERSION-gmp \
+        php$DOCKER_PHP_VERSION-imagick \
+        php$DOCKER_PHP_VERSION-imap \
+        php$DOCKER_PHP_VERSION-intl \
+        php$DOCKER_PHP_VERSION-json \
+        php$DOCKER_PHP_VERSION-ldap \
+        php$DOCKER_PHP_VERSION-readline \
+        php$DOCKER_PHP_VERSION-mbstring \
+        php$DOCKER_PHP_VERSION-mcrypt \
+        php$DOCKER_PHP_VERSION-memcached \
+        php$DOCKER_PHP_VERSION-mongodb \
+        php$DOCKER_PHP_VERSION-mysql \
+        php$DOCKER_PHP_VERSION-opcache \
+        php$DOCKER_PHP_VERSION-pgsql \
+        php$DOCKER_PHP_VERSION-redis \
+        php$DOCKER_PHP_VERSION-sqlite3 \
+        php$DOCKER_PHP_VERSION-ssh2 \
+        php$DOCKER_PHP_VERSION-xdebug \
+        php$DOCKER_PHP_VERSION-xml \
+        php$DOCKER_PHP_VERSION-xmlrpc \
+        php$DOCKER_PHP_VERSION-xsl \
+        php$DOCKER_PHP_VERSION-zip
+apt-get clean -qq
+apt-get autoremove -qq && ( -rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* >/dev/null 2>&1 || true ) \
+    && ( echo 'date.timezone="Europe/Vienna"' >> /etc/php/$DOCKER_PHP_VERSION/cli/conf.d/timezone.ini || true ) \
+    && rm -rf /etc/php/$DOCKER_PHP_VERSION/cli/conf.d/20-xdebug.ini
+
+
+########################################################################################################################
+# Php Phar Utils
+########################################################################################################################
+
+wget --no-verbose https://getcomposer.org/download/1.4.2/composer.phar --output-document=/usr/local/bin/composer \
+    && chmod +x /usr/local/bin/composer \
+    && composer self-update \
+    && composer --ansi global require hirak/prestissimo:^0.3 \
+    && composer --ansi global require fxp/composer-asset-plugin:~1.3 \
+    && composer --version
+
+wget --no-verbose https://symfony.com/installer --output-document=/usr/local/bin/symfony \
+    && chmod a+x /usr/local/bin/symfony \
+    && symfony --version
+
+wget --no-verbose https://github.com/consolidation/Robo/releases/download/1.1.2/robo.phar --output-document=/usr/local/bin/robo \
+    && chmod a+x /usr/local/bin/robo \
+    && robo --version
